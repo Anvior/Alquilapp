e8fc8af42d9ebbe287b9498a76924f156f90c729
Anartz Azumendi <anartz2001@gmail.com>
2025-07-19 14:23:36 +0200

    Primer commit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 
-const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -13,8 +9,6 @@ app.get('/', (req, res) => {
   res.send('API de AlquilApp funcionando 🚀');
 });
 
-// Puerto
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor backend escuchando en http://localhost:${PORT}`);
 });
